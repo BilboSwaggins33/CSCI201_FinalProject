d187635e9732f5f56ed9dd6619f08cabf8c00756
Aaron 'Da Possession Ends Here' Zhang <aarondzhang33@gmail.com>
2024-12-08 13:24:56 -0800

    ability to edit user profile using backend

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,82 +4,131 @@ import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react
 
 
 function Profile() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Post' },
-    { id: 2, title: 'Post' },
-    { id: 3, title: 'Post' },
-  ]);
+
+  const [posts, setPosts] = useState([]);
+
+  const [userInfo, setUserInfo] = useState();
 
   const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+
+
+    const fetchUserData = async () => {
+      // NOTE: Remove hardcoded id in the future
+      const userId = 1;
+      // NOTE: For now, this code fetches and stores in localStorage
+      // Will remove in the future once login functionality is finished
+      fetch(`http://localhost:8080/user/id/${userId}`, {
+        method: "GET",
+      }).then((response) => response.json()).then((data) => {
+        setUserInfo(data);
+      });
+
+
+
+      // NOTE: Can change this implementation later.
+      fetch(`http://localhost:8080/post/all`, {
+        method: "GET",
+      }).then((response) => response.json()).then((data) => {
+        const userPosts = data.filter((post, idx) => (post.user.id == userId));
+        setPosts(userPosts);
+        console.log(data);
+      });
+
+    }
+
+    fetchUserData();
+
+
+  }, []);
+
+
   const handleEditClick = () => {
     setIsEditing(true);
     console.log("Is Editing:", isEditing);
   };
   const handleCloseModal = () => {
     setIsEditing(false);
+    window.location.reload();
   };
-  const [profileData, setProfileData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    username: '',
-    school: '',
-    posts: [],
-  });
 
   const handleDelete = (postId) => {
     setPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
   };
 
-  
+
+  // <button className="edit-icon" onClick={handleEditClick}>✎</button>
+
 
   return (
-    
-        <div className="Profile">
-        
-            
-               
-            <header className="profile-header">Profile</header>
-            <div className="profile-content">
-                
-                <div className="profile-info">
-                <div className="profile-image-container">
-                    <img
-                    src="path-to-default-image.jpg"
-                    alt="profile pic?"
-                    className="profile-image"
-                    />
-                    <button className="edit-icon" onClick={handleEditClick}>✎</button>
-                </div>
-                <p className="profile-name">Name</p>
-                <p className="profile-username">username</p>
-                <p className="profile-school">University of Southern California</p>
-                </div>
 
-                
-                <div className="profile-details">
-                <div className="user-info">
-                    <p>Username: username</p>
-                    <p>Password: trojans1</p>
-                    <p>School: University of Southern California</p>
-                </div>
-                <div className="posts-section">
-                    {posts.map((post) => (
-                    <PostCard
-                        key={post.id}
-                        title={post.title}
-                        onDelete={() => handleDelete(post.id)}
-                        onEdit={() => {handleEditClick(post.id)}}
-                    />
-                    ))}
-                </div>
-                </div>
-                {isEditing && <Modal onClose={handleCloseModal} />}
-            </div>
-           
-           
+    <div className="Profile">
+      <header className="profile-header">Profile</header>
+      <div className="profile-content">
+
+        <div className="profile-info">
+          <div className="profile-image-container">
+            {/*<img
+              src="path-to-default-image.jpg"
+              alt="profile pic?"
+              className="profile-image"
+            />*/}
+          </div>
+          <p className="profile-name"></p>
+          <p className="profile-username">{userInfo?.fname} {userInfo?.lname}</p>
+          <p className="profile-school">{userInfo?.university}</p>
         </div>
-    
+
+
+        <div className="profile-details">
+          <div className="user-info">
+            <h2>User Information</h2>
+            <div className="profile-row">
+              <span>First Name</span>
+              <p>{userInfo?.fname}</p>
+            </div>
+
+            <div className="profile-row">
+              <span>Last Name</span>
+              <p>{userInfo?.lname}</p>
+            </div>
+
+            <div className="profile-row">
+              <span>Email</span>
+              <p>{userInfo?.email}</p>
+            </div>
+
+            <div className="profile-row">
+              <span>Password</span>
+              <p>{userInfo?.password}</p>
+            </div>
+
+            <div className="profile-row">
+              <span>University</span>
+              <p>{userInfo?.university}</p>
+            </div>
+
+
+            <button className="edit-button" onClick={handleEditClick}>Edit Profile</button>
+          </div>
+          <div className="posts-section">
+            {posts.map((post, idx) => (
+              <PostCard
+                key={post.postId}
+                title={post.name}
+                onDelete={() => handleDelete(post.postId)}
+                onEdit={() => { handleEditClick(post.postId) }}
+              />
+            ))}
+          </div>
+        </div>
+        {isEditing && <Modal data={userInfo} onClose={handleCloseModal} />}
+      </div>
+
+
+    </div>
+
   );
 }
 
@@ -97,107 +146,117 @@ function PostCard({ title, onDelete, onEdit }) {
 
 
 
-function Modal({ onClose }) {
+function Modal({ data, onClose }) {
 
-    const navigate = useNavigate(); 
-    const [formData, setFormData] = useState({
-      firstName: '',
-      lastName: '',
-      email: '',
-      username: '',
-      password: '',
-    });
-  
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    };
-  
-    const onSubmit = (e) => {
-      e.preventDefault(); 
-      const formValid = document.querySelector('form').checkValidity();
-      if (formValid) {
-        navigate('/sign-in');
-        
-        const jsonData = JSON.stringify(formData);
-        console.log('User Registration Data:', jsonData);
-        onClose();
-      } else {
-        alert('Please fill out the fields correctly.');
-      }
-    };
-  
-    return (
-      <div className="Modal-overlay">
-        <div className="Modal">
-          <h3>To edit your account, enter new information below</h3>
-          <form onSubmit={onSubmit}>
-            <div className="Form-group">
-              <label>First Name:</label>
-              <input
-                type="text"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleChange}
-                placeholder="First"
-                required
-              />
-            </div>
-            <div className="Form-group">
-              <label>Last Name:</label>
-              <input
-                type="text"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-                placeholder="Last"
-                required
-              />
-            </div>
-            <div className="Form-group">
-              <label>Email:</label>
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email"
-                required
-              />
-            </div>
-            <div className="Form-group">
-              <label>Username:</label>
-              <input
-                type="text"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-                placeholder="User"
-                required
-              />
-            </div>
-            <div className="Form-group">
-              <label>Password:</label>
-              <input
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                placeholder="************"
-                required
-              />
-            </div>
-  
-  
-            <button className="Button2" type="submit">Submit</button>
-          </form>
-          <button className="Close-button" onClick={onClose}>Close</button>
-        </div>
-      </div>
-    );
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState(data);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formValid = document.querySelector('form').checkValidity();
+    if (formValid) {
+      // navigate('/sign-in');
+      const jsonData = JSON.stringify(formData);
+      console.log('User Registration Data:', jsonData);
+      const userId = data.id;
+      fetch("http://localhost:8080/user/update", {
+        method: "POST",
+        body: jsonData,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then((response) => response.json()).then((resp) => {
+        console.log(resp);
+        onClose();
+      });
+    } else {
+      alert('Please fill out the fields correctly.');
+    }
+  };
+
+  return (
+    <div className="Modal-overlay">
+      <div className="Modal">
+        <h3>To edit your account, enter new information below</h3>
+        <form onSubmit={onSubmit}>
+          <div className="Form-group">
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="fname"
+              value={formData.fname}
+              onChange={handleChange}
+              placeholder="First"
+            />
+          </div>
+          <div className="Form-group">
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="lname"
+              value={formData.lname}
+              onChange={handleChange}
+              placeholder="Last"
+            />
+          </div>
+          <div className="Form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+            />
+          </div>
+          <div className="Form-group">
+            <label>Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder="User"
+            />
+          </div>
+          <div className="Form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="************"
+            />
+          </div>
+          <div className="Form-group">
+            <label>University:</label>
+            <input
+              type="text"
+              name="university"
+              value={formData.university}
+              onChange={handleChange}
+              placeholder="University"
+            />
+          </div>
+
+
+
+
+          <button className="Button2" type="submit">Save</button>
+        </form>
+        <button className="Close-button" onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
